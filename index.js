d967ea2e979afdd09b79c80de85061778adcf661
rebahaider <rebahaiderema@gmail.com>
2024-06-11 12:13:03 +0600

    creating note update api

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ async function run() {
             const result = await notes.toArray();
             res.send(result);
         })
+        app.get('/notes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await notesCollection.findOne(query);
+            res.send(result);
+        })
         app.delete('/notes/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
